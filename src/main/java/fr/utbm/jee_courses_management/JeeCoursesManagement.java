d45d95c6b10422f3e4af0aaafa837d10ec1907b4
macdrien <adrien.bouyssou@gmail.com>
2020-10-28 11:25:19 +0100

    [ADD] Maven project initialization

---
+++ src/main/java/fr/utbm/jee_courses_management/JeeCoursesManagement.java
@@ -0,0 +1,8 @@
+package fr.utbm.jee_courses_management;
+
+public class JeeCoursesManagement {
+
+    public static void main(String[] args) {
+        System.out.println("[INFO] Start JEE Courses Management");
+    }
+}
---
+++ src/test/java/fr/utbm/jee_courses_management/JeeCoursesManagementTest.java
@@ -0,0 +1,17 @@
+package fr.utbm.jee_courses_management;
+
+import org.junit.Test;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("fr.utbm.jee_courses_management")
+public class JeeCoursesManagementTest {
+
+    @Test
+    public void mainTest() {
+        fail("Not yet implemented");
+    }
+}
