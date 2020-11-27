636a3f033c81b125ebb37a1898a1c8ea69fe6c80
abouysso <adrien.bouyssou@utbm.fr>
2020-11-27 08:49:05 +0100

    [ADD] Get all courses function

---
+++ core/src/main/java/fr/utbm/jee_courses_management/repository/CourseRepository.java
@@ -0,0 +1,36 @@
+package fr.utbm.jee_courses_management.repository;
+
+
+import fr.utbm.jee_courses_management.entity.Course;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseRepository {
+
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("courses-management");
+    EntityManager entityManager = null;
+
+    /**
+     * Get all courses from the entity {@link Course}
+     *
+     * @return A {List} containing all found courses. The returned List can be empty but it cannot be null.
+     */
+    public List<Course> getCourses() {
+        if (entityManager == null)
+            entityManager = entityManagerFactory.createEntityManager();
+
+        List<Course> courses = new ArrayList<>();
+        for (Object result : entityManager.createQuery("from Course").getResultList())
+            try {
+                courses.add((Course) result);
+            } catch (ClassCastException exception) {
+                System.out.println("The object " + result.toString() + " cannot be casted");
+            }
+
+        return courses;
+    }
+}
