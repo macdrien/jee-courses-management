7f602d3f8899f146fd767a550fab9b33c1a7c398
abouysso <adrien.bouyssou@utbm.fr>
2020-11-29 15:17:36 +0100

    [ADD] EntityManagerFactory

--- core/src/main/java/fr/utbm/jee_courses_management/repository/CourseRepository.java
+++ core/src/main/java/fr/utbm/jee_courses_management/repository/CourseRepository.java
@@ -1,18 +1,20 @@
 package fr.utbm.jee_courses_management.repository;
 
-
 import fr.utbm.jee_courses_management.entity.Course;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CourseRepository {
 
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("courses-management");
-    EntityManager entityManager = null;
+public class CourseRepository implements Serializable {
+
+    private EntityManager entityManager;
+
+    public CourseRepository() {
+        entityManager = EntityManagerFactory.getEntityManager();
+    }
 
     /**
      * Get all courses from the entity {@link Course}
@@ -20,17 +22,14 @@ public class CourseRepository {
      * @return A {List} containing all found courses. The returned List can be empty but it cannot be null.
      */
     public List<Course> getCourses() {
-        if (entityManager == null)
-            entityManager = entityManagerFactory.createEntityManager();
-
         List<Course> courses = new ArrayList<>();
-        for (Object result : entityManager.createQuery("from Course").getResultList())
-            try {
+        for (Object result : entityManager.createQuery("from Course").getResultList()) {
+            if (result instanceof Course) {
                 entityManager.detach(result);
-                courses.add((Course) result);
-            } catch (ClassCastException exception) {
-                System.out.println("The object " + result.toString() + " cannot be casted");
+                Course course = (Course) result;
+                courses.add(course);
             }
+        }
 
         return courses;
     }
@@ -41,9 +40,6 @@ public class CourseRepository {
      * @param course The {@link Course} to save.
      */
     public void save(Course course) {
-        if (entityManager == null)
-            entityManager = entityManagerFactory.createEntityManager();
-
         entityManager.getTransaction().begin();
         entityManager.merge(course);
         entityManager.getTransaction().commit();
