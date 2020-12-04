323c4c093d23a99519a0d4b854b8f80e8bb54583
abouysso <adrien.bouyssou@utbm.fr>
2020-12-04 21:43:01 +0100

    [ADD] All Filter.filterCoursesAndSession()
    Comment: Bug fixes are comming in next commits

--- core/src/test/java/fr/utbm/jee_courses_management/util/FilterTest.java
+++ core/src/test/java/fr/utbm/jee_courses_management/util/FilterTest.java
@@ -35,6 +35,230 @@ public class FilterTest {
         courseSessionRepository = new CourseSessionRepository();
     }
 
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    // TODO Add null courses test
+    @Test
+    public void testFilterCoursesAndSessionsWithNullCoursesList() {
+        assertNull(Filter.filterCoursesAndSessions(null, null));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    // TODO Add empty list test
+    @Test
+    public void testFilterCoursesAndSessionsWithEmptyCoursesList() {
+        assertEquals(List.of(), Filter.filterCoursesAndSessions(List.of(), null));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithNullFilter() {
+        List<Course> courses = courseRepository.getCourses(false);
+        assertThrows(NullPointerException.class, () -> {Filter.filterCoursesAndSessions(courses, null); });
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithoutFilteringOptions() {
+        Filter filter = new Filter();
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithBlankKeywordOptionAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setKeyword("");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithKeywordTakingAllCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        // In the test data, "5" is in all course title
+        filter.setKeyword("5");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithKeywordTakingNoCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setKeyword("Not contained in any course");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(List.of(), Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithKeywordTakingSomeCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        /*
+         * Courses titles are "LO54", "AD50" and "SR50".
+         * Index:              0    ,  1     and  2
+         *
+         * The keyword "50" will exclude "LO54" and keep the two others.
+         */
+        filter.setKeyword("50");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(List.of(courses.get(1), courses.get(2)),
+                Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullStartingDateTakingAllCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setStartingDate(LocalDate.MIN);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    // TODO fix it by removing all courses which do not have any session
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullStartingDateTakingNoCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setStartingDate(LocalDate.MAX);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(List.of(), Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullStartingDateTakingSomeCoursesAndNoOtherOptions() {
+        LocalDate startingDate = LocalDate.of(2020, 6, 1);
+        Filter filter = new Filter();
+        // That will exclude which begins in the 2020's first semester or before
+        filter.setStartingDate(startingDate);
+
+       List<Course> courses = courseRepository.getCourses(true);
+
+        for (Course course: Filter.filterCoursesAndSessions(courses, filter))
+            course.getSessions().forEach(session -> {
+                    if (0 < startingDate.compareTo(session.getStartingDate()))
+                        fail("The session " + session.getId() + " which starts on " + session.getStartingDate() +  " must be excluded but it is not");
+            });
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullEndingDateTakingAllCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setEndingDate(LocalDate.MAX);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    // TODO Fix with more research
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullEndingDateTakingNoCoursesAndNoOtherOptions() {
+        Filter filter = new Filter();
+        filter.setEndingDate(LocalDate.MIN);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(List.of(), Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithNonNullEndingDateTakingSomeCoursesAndNoOtherOptions() {
+        LocalDate endingDate = LocalDate.of(2021, 1, 1);
+        Filter filter = new Filter();
+        // That will exclude which begins in the 2020's first semester or before
+        filter.setEndingDate(endingDate);
+
+        List<Course> courses = courseRepository.getCourses(true);
+
+        for (Course course: Filter.filterCoursesAndSessions(courses, filter))
+            course.getSessions().forEach(session -> {
+                if (endingDate.compareTo(session.getStartingDate()) < 0)
+                    fail("The session " + session.getId() + " which finishes on " + session.getEndingDate() +  " must be excluded but it is not");
+            });
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithBlankCityAndNoOtherOption() {
+        Filter filter = new Filter();
+        filter.setCity("");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithCityTakingAllSessionsAndNoOtherOption() {
+        Filter filter = new Filter();
+        // Available cities are "Mulhouse", "Belfort" and "Montbeliard"
+        filter.setCity("e");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(courses, Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    // cf comment on {@link FilterTest#testFilterCoursesAndSessionsWithNonNullStartingDateTakingNoCoursesAndNoOtherOptions()}
+    @Test
+    public void testFilterCoursesAndSessionsWithCityTakingNoSessionsAndNoOtherOption() {
+        Filter filter = new Filter();
+        // Available cities are "Mulhouse", "Belfort" and "Montbeliard"
+        filter.setCity("z");
+        List<Course> courses = courseRepository.getCourses(true);
+
+        assertEquals(List.of(), Filter.filterCoursesAndSessions(courses, filter));
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWithCityTakingSomeSessionsAndNoOtherOption() {
+        final String SEARCHED_NAME = "M";
+        Filter filter = new Filter();
+        // Available cities are "Mulhouse", "Belfort" and "Montbeliard"
+        filter.setCity(SEARCHED_NAME);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        Filter.filterCoursesAndSessions(courses, filter).forEach(course -> {
+            course.getSessions().forEach(session -> {
+                if (!session.getLocation().getCity().contains(SEARCHED_NAME))
+                    fail("The session " + session.getId() + " taking place in/at " + session.getLocation().getCity() + " must be excluded but it is not");
+            });
+        });
+    }
+
+    /** Test method for {@link Filter#filterCoursesAndSessions(List, Filter)} */
+    @Test
+    public void testFilterCoursesAndSessionsWhichAreIn2020Only() {
+        final LocalDate BEGINNING_OF_2020 = LocalDate.of(2020, 1, 1),
+                ENDING_OF_2020 = LocalDate.of(2020, 12, 31);
+        Filter filter = new Filter(null, BEGINNING_OF_2020,
+                                    ENDING_OF_2020, null);
+        List<Course> courses = courseRepository.getCourses(true);
+
+        Filter.filterCoursesAndSessions(courses, filter).forEach(course -> {
+            course.getSessions().forEach(session -> {
+                if (session.getStartingDate().isBefore(BEGINNING_OF_2020))
+                    fail("The session " + session.getId() + " beginning the " + session.getStartingDate() + " must be excluded but it is not");
+
+                if (session.getEndingDate().isAfter(ENDING_OF_2020))
+                    fail("The session " + session.getId() + " ending the " + session.getEndingDate() + " must be excluded but it is not");
+            });
+        });
+    }
+
     /** Test method for {@link Filter#filterCourseTitle(Course, String)} */
     @Test
     public void testFilterCourseTitle() {
