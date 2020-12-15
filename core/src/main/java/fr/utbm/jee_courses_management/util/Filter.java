3e36edce54445fbb257b4aae5e4eef65032c6145
abouysso <adrien.bouyssou@utbm.fr>
2020-12-15 21:56:06 +0100

    [REF] Clean up on Filter

--- core/src/main/java/fr/utbm/jee_courses_management/util/Filter.java
+++ core/src/main/java/fr/utbm/jee_courses_management/util/Filter.java
@@ -11,6 +11,12 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A class representing a filter to apply on a {@link CourseSession} research.
+ *
+ * It is possible to filter the {@link Course}, the {@link fr.utbm.jee_courses_management.entity.Location} and
+ * the starting and ending dates of the {@link CourseSession}.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,22 +25,22 @@ public class Filter implements Serializable {
     /** A keyword which must be in the {@link fr.utbm.jee_courses_management.entity.Course} title */
     private String keyword;
 
-    /** The minimum date to begin the session */
+    /** The minimum date to begin the {@link CourseSession} */
     private LocalDate startingDate;
 
-    /** The minimum date to terminate the session */
+    /** The minimum date to terminate the {@link CourseSession} */
     private LocalDate endingDate;
 
-    /** The name of the city where the session have to take place */
+    /** The name of the city in where the {@link CourseSession} have to take place */
     private String city;
 
     /**
      * Filter a {@link List} of {@link Course} and their {@link CourseSession} following criteria in the given {@link Filter}.
-     * If a course has no session. It will not be kept.
+     * If a {@link Course} has no {@link CourseSession}. It will not be kept.
      *
-     * @param courses The courses to filter
-     * @param filter The filter to apply. It must be not null.
-     * @return The filtered list of courses with the filtered sessions.
+     * @param courses The {@link List} of {@link Course} with their {@link CourseSession} to filter
+     * @param filter The {@link Filter} to apply. It must be not null.
+     * @return The filtered {@link List} of {@link Course} with their filtered {@link CourseSession}.
      */
     public static List<Course> filterCoursesAndSessions(List<Course> courses, Filter filter) {
         if (courses == null || courses.isEmpty())
@@ -60,23 +66,21 @@ public class Filter implements Serializable {
                     )).collect(Collectors.toList())
             ));
 
-        // Remove all courses which have no session
-        courses = courses.stream()
+        // Remove all courses which have no session and return the last list
+        return courses.stream()
                 .filter(course -> course.getSessions().size() != 0)
                 .collect(Collectors.toList());
-
-        return courses;
     }
 
     /**
      * Test if a {@link Course} can be kept by testing if its title contains a given keyword.
      *
-     * @param course The course to test.
-     * @param keyword The keyword to search in the course title. It must be not null and not blank.
+     * @param course The {@link Course} to test.
+     * @param keyword The keyword to search in the {@link Course} title. It must be not null and not blank.
      * @return The result of the test:
      * <ul>
-     *     <li>true if the course title contains keyword</li>
-     *     <li>false if keyword is not in the course title</li>
+     *     <li>true if the {@link Course} title contains keyword</li>
+     *     <li>false if keyword is not in the {@link Course} title</li>
      * </ul>
      */
     public static boolean filterCourseTitle(Course course, String keyword) {
@@ -86,12 +90,12 @@ public class Filter implements Serializable {
     /**
      * Test if a {@link CourseSession} can be kept by testing if its begin and ending are in the given interval.
      *
-     * @param session The session to test.
-     * @param startingDate The minimum date to begin the session. It has to be not null.
+     * @param session The {@link CourseSession} to test.
+     * @param startingDate The minimum date to begin the {@link CourseSession}. It has to be not null.
      * @return The result of the test:
      * <ul>
-     *     <li>true if the session begin in or before the given date</li>
-     *     <li>false if startingDate is null or if the session begins before it.</li>
+     *     <li>true if the {@link CourseSession} begin in or before the given date</li>
+     *     <li>false if startingDate is null or if the {@link CourseSession} begins before it.</li>
      * </ul>
      */
     public static boolean filterSessionBeginAfter(CourseSession session, LocalDate startingDate) {
@@ -101,12 +105,12 @@ public class Filter implements Serializable {
     /**
      * Test if a {@link CourseSession} can be kept by testing if its begin and ending are in the given interval.
      *
-     * @param session The session to test.
-     * @param endingDate The maximum date to finish the session. It has to be not null.
+     * @param session The {@link CourseSession} to test.
+     * @param endingDate The maximum date to finish the {@link CourseSession}. It has to be not null.
      * @return The result of the test:
      * <ul>
-     *     <li>true if the session finish in or after the given date</li>
-     *     <li>false if endingDate is null or if the session finish after it.</li>
+     *     <li>true if the {@link CourseSession} finish in or after the given date</li>
+     *     <li>false if endingDate is null or if the {@link CourseSession} finish after it.</li>
      * </ul>
      */
     public static boolean filterSessionFinishBefore(CourseSession session, LocalDate endingDate) {
@@ -116,12 +120,12 @@ public class Filter implements Serializable {
     /**
      * Test if a {@link CourseSession} can be kept by testing if it takes place in the given city.
      *
-     * @param session The session to test.
-     * @param city The city where the session has to take place. It must be not null and not blank.
+     * @param session The {@link CourseSession} to test.
+     * @param city The city where the {@link CourseSession} has to take place. It must be not null and not blank.
      * @return The result of the test:
      * <ul>
-     *     <li>true if the takes place in the given city.</li>
-     *     <li>false if the session does not takes place in the given city</li>
+     *     <li>true if the{@link CourseSession} takes place in the given city.</li>
+     *     <li>false if the {@link CourseSession} does not takes place in the given city</li>
      * </ul>
      */
     public static boolean filterSessionInCity(CourseSession session, String city) {
